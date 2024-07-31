eea3c7ac508e0359781bf4d0ca4ba11a2ae79745
Asur-Daanav <542akhil@gmail.com>
2024-08-01 04:44:40 +0530

    added files

---
+++ login.js
@@ -0,0 +1,86 @@
+
+// Login page Logic starts
+
+// these two lines of code fetches all emails and passwords from the local storage
+const emails = JSON.parse(localStorage.getItem('emails')) || [];
+const passwords = JSON.parse(localStorage.getItem('passwords')) || [];
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form from submitting
+
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let errorMessage = document.getElementById('errorMessage');
+    let button = document.getElementById('loginBtn');
+
+    // Basic validation
+    if (!email || !password) {
+        errorMessage.textContent = 'Please enter both email and password.';
+        return;
+    }
+
+    // Simulate server-side validation
+
+    let emailFlag = false;
+    let passwordFlag = false;
+    let emailPasswordFlag = false;
+
+    // this for loop iterates the emails and passwords in the localStorage and match the user's email password
+    for (let i = 0; i < emails.length; i++) {
+      if (email === emails[i] && password === passwords[i]) {
+        emailFlag = false;
+        passwordFlag = false;
+        emailPasswordFlag = false;
+        window.open('./homeDummy.html');
+        break;
+        // Redirect to another page or perform other actions upon successful login
+      }
+      else{
+        if(email !== emails[i] && password === passwords[i]) {
+          emailFlag = true;
+        }
+        if(email === emails[i] && password !== passwords[i]) {
+          passwordFlag = true;
+        }
+        if(email !== emails[i] && password !== passwords[i]){
+          emailPasswordFlag = true;
+        }
+      }
+      
+    }
+    if(emails.length == 0){
+      errorMessage.textContent = 'Account does not exist\nPlease register.';
+    }
+    
+    if(emailPasswordFlag){
+      errorMessage.textContent = 'Account does not exist\nPlease register.';
+    }
+    if(emailFlag){
+      errorMessage.textContent = 'Invalid email.';
+    }
+    if(passwordFlag){
+      errorMessage.textContent = 'Invalid password.';
+    }
+     
+});
+
+// Add password visibility toggle
+const passwordInput = document.getElementById('password');
+
+const passwordToggle = document.createElement('span');
+passwordToggle.innerHTML = '👀';
+passwordToggle.classList.add('text-gray-600', 'cursor-pointer');
+passwordInput.parentNode.appendChild(passwordToggle);
+
+passwordToggle.addEventListener('click', () => {
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    passwordToggle.innerHTML = '🙈';
+  } else {
+    passwordInput.type = 'password';
+    passwordToggle.innerHTML = '👀';
+  }
+});
+
+// Login page Logic ends
+// responsive logic 
---
+++ register.js
@@ -0,0 +1,86 @@
+// registration page code starts here
+let names;
+let emails;
+let passwords;
+document.querySelector('#registerForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form from submitting
+    
+    let email = document.getElementById('email').value;
+    let name = document.getElementById('name').value;
+    let password = document.getElementById('password').value;
+    let confirmPassword = document.getElementById('confirmPassword').value;
+    let errorMessage = document.getElementById('errorMessage');
+
+    // Basic validation
+    if (!email || !password || !confirmPassword) {
+        errorMessage.textContent = 'Please enter email,password and confirm password.';
+        return;
+    }
+    if(password != confirmPassword){
+        errorMessage.textContent = 'Password and confirm password does not match.';
+    }
+    else{
+        showSuccessModal();
+        console.log("working");
+    }
+  
+     // Get existing values from localStorage
+     names = JSON.parse(localStorage.getItem('names')) || [];
+     emails = JSON.parse(localStorage.getItem('emails')) || [];
+     passwords = JSON.parse(localStorage.getItem('passwords')) || [];
+
+     // Add the new value to the array
+     names.push(name);
+     emails.push(email);
+     passwords.push(password);
+
+     // Store the updated array back to localStorage
+     localStorage.setItem('names', JSON.stringify(names));
+     localStorage.setItem('emails', JSON.stringify(emails));
+     localStorage.setItem('passwords', JSON.stringify(passwords));
+    
+}); 
+
+// this will clear the localStorage mtlb deletes all emails and passwords from the storage
+localStorage.clear();
+
+// Add password visibility toggle
+const passwordInput = document.getElementById('password');
+
+const passwordToggle = document.createElement('span');
+passwordToggle.innerHTML = '👀';
+passwordToggle.classList.add('text-gray-600', 'cursor-pointer');
+passwordInput.parentNode.appendChild(passwordToggle);
+
+passwordToggle.addEventListener('click', () => {
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    passwordToggle.innerHTML = '🙈';
+  } else {
+    passwordInput.type = 'password';
+    passwordToggle.innerHTML = '👀';
+  }
+});
+
+// register Success
+// Helper function to display the success modal
+function showSuccessModal() {
+  const modal = document.getElementById('success-modal');
+  const closeModalButton = document.getElementById('close-modal');
+
+  // Show the modal
+  modal.classList.add('show');
+
+  // Hide the modal when the close button is clicked
+  closeModalButton.addEventListener('click', () => {
+    modal.classList.remove('show');
+    window.open('login.html');
+  });
+
+  // Hide the modal when clicking outside of the modal content
+  window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+      modal.classList.remove('show');
+    }
+  });
+}
